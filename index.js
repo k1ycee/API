cc91ce027d70d1fbecf3d410a9f1e568bf200fc5
Denzel <chiagozieani145@gmail.com>
2020-06-06 04:42:26 +0100

    FCC Mongoose and MongoDB challenge

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 
+
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
